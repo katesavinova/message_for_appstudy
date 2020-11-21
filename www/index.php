0870c744597c0538b07d823164c70d0e33897c25
Ekaterina <savinova.katerina@inbox.ru>
2020-11-21 11:24:44 +0400

    Page about  APP Study for sent email

---
+++ www/index.php
@@ -0,0 +1,13 @@
+<?header('Content-Type: text/html; charset= utf-8');?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Сообщения | Предложения</title>
+</head>
+<body>
+    <p>Ваши вопросы и предложения</p>
+    <a href="https://katesavinova.github.io/app-for-study/">Перейти в приложение</a>
+</body>
+</html>
